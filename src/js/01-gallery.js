4ff0613c7f06423e9924ba7c58c107ef2fa6fd68
Victor8825 <victorrv2588@gmail.com>
2022-06-09 17:25:35 +0300

    task 1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function createGalleryItemMarkup(gallery) {
+ return gallery.map( ({ preview, original, description }) => {
+  return `<a class="gallery__item" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       alt="${description}"
+       title='dfdsfsdfsdf'
+     />
+   </a>
+ `;
+ })
+ .join("")
+}
+
+const modalGalleryOptions = {
+  captionsData: 'alt',
+  captionDelay: 250
+}
+const modalGallery = new SimpleLightbox(".gallery a", modalGalleryOptions );
+
+galleryContainerRef.addEventListener("click", onImageClick);
